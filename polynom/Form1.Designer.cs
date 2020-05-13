ace630e8a53a9a8c2c61305a0933590a70e63bb1
chicot1k <58642288+chicot1k@users.noreply.github.com>
2020-05-13 14:45:57 +0300

    Add files via upload

---
+++ polynom/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace polynom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(721, 361);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(433, 404);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(513, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите коэффиценты";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(619, 458);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(433, 458);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Границы ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(657, 481);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "До";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(463, 481);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "От";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 503);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ polynom/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polynom
+{
+    public partial class Form1 : Form
+    {
+        float polynom(float x, float[] k)
+        {
+            float result=0;
+            for(int i = 0; i < k.Length; i++)
+            {
+                result += k[i] * (float)Math.Pow(x, k.Length - 1 - i);
+            }
+            return result;
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            float[] k;
+            float a, b;
+            k = textBox1.Text.Split(' ').Select(float.Parse).ToArray();
+            float max_cord_x=10, min_cord_x=-10, max_cord_y=10, min_cord_y=-10, step_cord_x=2, step_cord_y=2;
+            
+            b = float.Parse(textBox3.Text);
+            a = float.Parse(textBox2.Text);
+            if (a > b)
+            {
+                float temp = a;
+                a = b;
+                b = temp;
+            }
+               
+            while (a < min_cord_x || b > max_cord_x)
+            {
+                max_cord_x *= 2;
+                min_cord_x *= 2;
+                step_cord_x *= 2;
+               
+            }
+            while(a>min_cord_x+4*step_cord_x && b < max_cord_x - 4*step_cord_x)
+            {
+                max_cord_x /= 2;
+                min_cord_x /= 2;
+                step_cord_x /= 2;
+
+            }
+            Bitmap picture = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics de = Graphics.FromImage(picture);
+            pictureBox1.Image = picture;
+            Pen ruchka = new Pen(Color.Black, 1f);
+            de.DrawLine(ruchka, 0, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height / 2);
+            de.DrawLine(ruchka, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+            var shrift = new Font(Font.FontFamily, 8);
+            for (int i=0; i<11; i++)
+            {
+                if (i != 5)
+                {
+                    de.DrawLine(ruchka, pictureBox1.Width / 10 * i, pictureBox1.Height / 2 - 3, pictureBox1.Width / 10 * i, pictureBox1.Height / 2 + 3);
+                    float x = min_cord_x + i * step_cord_x;
+                    de.DrawString(x.ToString(), shrift, Brushes.Black, pictureBox1.Width / 10 * i, pictureBox1.Height / 2 + 10);
+                }
+            }
+            de.DrawString(max_cord_x.ToString(), shrift, Brushes.Black, pictureBox1.Width-20, pictureBox1.Height / 2 +10);
+            float y_min = 10000000000000, y_max = -9999999999999999 ;
+            float step_x = (max_cord_x - min_cord_x) / pictureBox1.Width;
+            for (int i=0; i < pictureBox1.Width; i++)
+            {
+                if (polynom(min_cord_x+i * step_x, k) > y_max)
+                {
+                    y_max = polynom(min_cord_x + i * step_x, k);
+                }
+                if (polynom(min_cord_x+i * step_x, k) < y_min)
+                {
+                    y_min = polynom(min_cord_x + i * step_x, k);
+                }
+
+            }
+            while (y_min < min_cord_y || y_max > max_cord_y)
+            {
+                max_cord_y *= 2;
+                min_cord_y *= 2;
+                step_cord_y *= 2;
+
+            }
+            while (y_min > min_cord_y + 4 * step_cord_y && y_max < max_cord_y - 4 * step_cord_y)
+            {
+                max_cord_y /= 2;
+                min_cord_y /= 2;
+                step_cord_y /= 2;
+
+            }
+            
+            for (int i = 0; i < 11; i++)
+            {
+                if (i != 5)
+                {
+                    de.DrawLine(ruchka, pictureBox1.Width / 2 - 3, pictureBox1.Height / 10*i, pictureBox1.Width / 2 + 3, pictureBox1.Height / 10*i);
+                    float y = (min_cord_y + i * step_cord_y)*-1;
+                    de.DrawString(y.ToString(), shrift, Brushes.Black, pictureBox1.Width/2 - 30,i*pictureBox1.Height/10) ;
+                    
+                }
+                de.DrawString((-1*max_cord_y).ToString(), shrift, Brushes.Black, pictureBox1.Width/2 - 30,pictureBox1.Height-15) ;
+            }
+            float cur_x=min_cord_x, cur_y=polynom(cur_x,k), next_x, next_y;
+            
+            float ky =pictureBox1.Height / (max_cord_y - min_cord_y);
+            for (int i = 0; i < pictureBox1.Width - 1; i++)
+            {
+                next_x = cur_x + step_x;
+                next_y = polynom(next_x, k);
+                ruchka.Color = Color.Red;
+                de.DrawLine(ruchka, i, pictureBox1.Height / 2 - ky * cur_y, i+1, pictureBox1.Height / 2 - ky * next_y);
+                cur_x = next_x;
+                cur_y = next_y;
+
+            }
+        }
+
+        
+    }
+}
